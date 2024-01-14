7aa5b2e8e163b3af0991ec9cffb927b9f3c71fb4
Paweł Staniul <pstaniul@gmail.com>
2024-01-14 07:27:52 +0100

    Utworzono główną stronę index.php

---
+++ index_old.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- czy to jest potrzebne?-->
+    <title> Wyszukiwarka kulturalna </title>
+    <link rel="icon" type="image/x-icon" href="favicon1.ico"> <!-- popraw te nutki -->
+
+    <meta name="description" content="KONCETRY, TEATR, BALET, OPERA" /> <!-- możesz wydłużyć ten opis -->
+    <meta name="keywords" content="wyszukiwarka koncertów, koncerty, wydarzenia kulturalne, teatr, BALET, OPERA" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" /> <!-- renderowanie strony w najwyższej dostępnej wersji Internet Explorer-->
+
+    <link rel="stylesheet" href="/public/css/style.css"/>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,400;1,100&display=swap" rel="stylesheet">
+
+
+
+
+</head>
+
+<body>
+
+    <div id="container">
+
+        <!-- <div class="col-md-4 col-sm-4> -->
+
+        <div id="logo" class="form-control">
+            Wyszukiwarka koncertów i wydarzeń
+        </div>
+
+        <div class="rectangle">
+
+            
+
+            <div id="email">
+                <a href="http://mail.google.com" target="_blank" class="sociallink" title="KONTAKT"><i class="icon-mail"></i></a>
+            </div>
+
+            <div style="clear:both;"></div>
+
+
+        </div>
+
+
+        <div id="nav">
+
+            <div class="optionL"> <a href="1 koncerty.html" class="navlink">Koncerty</a> </div>
+            <div class="optionL"> <a href="2 teatr.html" class="navlink">Teatr </a> </div>
+            <div class="optionL"> <a href="3 balet.html" class="navlink">Balet </a> </div>
+            <div class="optionL"> <a href="4 opera .html" class="navlink">Opera </a> </div>
+            <div class="optionL"> <a href="5 kino.html" class="navlink">Kino </a> </div> <br /> <br />
+
+            <div class="profil"> <a href="6 profil.html" class="navlink">Profil użytkownika </a> </div>
+
+
+        </div>
+
+        <div id="content">
+
+            <h2> Odkryj serce kultury! &hearts; Zanurz się w bogactwie wydarzeń artystycznych! </h2> <br>
+
+            <div id="search">
+                <form role="search" method="get" class="search-form" action="index1.html">
+                    <label>
+                        <span> &#x1F50E;&#xFE0E;</span>
+                        <input type="search" id="text" placeholder="Wyszukaj..." value name="s">
+                        <button type="submit" id="button">
+                            GO!
+                        </button>
+                    </label>
+                </form>
+
+
+            </div> <br><br>
+
+
+
+
+
+
+            <!--
+            <article>  Wydarzenia kulturalne w Polsce obejmują szeroki wachlarz działań,    odzwierciedlających bogatą i różnorodną kulturę kraju. W całym kraju odbywają się liczne festiwale muzyczne, które często koncentrują się na konkretnych gatunkach, od jazzu po muzykę elektroniczną, przyciągając zarówno lokalnych, jak i międzynarodowych artystów.  Polskie muzea i galerie regularnie organizują wystawy sztuki, prezentując zarówno dzieła współczesne, jak i klasyczne. Teatry i opery oferują bogaty repertuar, od tradycyjnych do nowoczesnych produkcji, podobnie jak festiwale filmowe, które są platformą dla młodych i niezależnych twórców. Wydarzenia literackie, takie jak festiwale książki, promują czytelnictwo, oferując spotkania z autorami i dyskusje panelowe. Tradycyjne polskie rzemiosło i sztuka ludowa są prezentowane na targach i rynkach, gdzie można nabyć unikalne przedmioty. Festiwale historyczne i folklorystyczne celebrują bogatą historię Polski i regionalne tradycje. Koncerty i recitale, od kameralnych występów po duże imprezy na otwartym powietrzu, prezentują różnorodność muzyczną. Festiwale kulinarne z kolei promują polską kuchnię regionalną i międzynarodową. Istnieją również wydarzenia interaktywne i edukacyjne, takie jak warsztaty i kursy, które promują naukę i rozwój osobisty w dziedzinie kultury. Każde z tych wydarzeń oferuje unikalne doświadczenia, umożliwiając zarówno mieszkańcom, jak i odwiedzającym zgłębienie bogactwa kultury i dziedzictwa Polski.  <br> 
+            </article> -->
+
+            <div class="event">
+                <div class="eventL" style="float: left;"> <img src="img/katie.jpg" height="140" /> </div>
+                <div class="tekstR"> <br>
+                    <strong> KATIE MELUA </strong> sprawnie łączy w swoich piosenkach wpływy muzyki pop, folku, bluesa i jazzowego wokalu. Jej brzmienie porównuje się do stylistyki utworów Nory Jones czy Diany Krall.<br><br>
+                    DATA // TYP wydarzenia // Miasto?
+
+                </div>
+
+            </div> <br>
+
+            <div class="event">
+                <div class="eventL" style="float: left;"> <img src="img/donjouan.jpg" height="140" /> </div>
+                <div class="tekstR"> <br>
+                    Jedna z najbardziej zniewalających postaci kultury zachodniej – Don Juan – nadal inspiruje twórców. O przyczynę donżuanizmu pytają pisarze, twórcy filmowi i choreografowie. Czy w naszej zseksualizowanej rzeczywistość i przy wszechobecnej pornografii zachowania w typie uwodziciela z Sewilli jeszcze szokują? Gdzie ukrywa się miłość, czułość i zrozumienie? Opowieść o Don Juanie – inspirująca powstanie wielu dzieł sztuki – nadal potrafi zaskakiwać. </div>
+
+            </div> <br>
+
+            <div class="event">
+                <div class="eventL" style="float: left;"> <img src="img/donkichot.jpg" height="140" /> </div>
+                <div class="tekstR"><br>
+                    Temperament, wdzięk, wirtuozeria oraz potężna dawka komizmu – baletowa wersja przygód szlachetnego rycerza z La Manchy od dawna urzeka miłośników tańca. Ten oparty na znakomitym literackim pierwowzorze spektakl w choreografii Michala Štípy przenosi nas do słonecznej Hiszpanii, gdzie piękna Kitri i zakochany w niej Basilio napotykają zbłąkanego rycerza, któremu towarzyszy nieodłączny giermek. </div>
+
+            </div><br>
+
+
+
+            <div class="event">
+                <div class="eventL" style="float: left;"> <img src="img/kroke.jpg" height="140" /> </div>
+                <div class="tekstR"> <br>
+                    <strong>KROKE - </strong> Swoją muzyką zachwycają nas już od 28 lat! Wszystko zawdzięczają swoim talentom i pracy, ale kto wie jak potoczyłaby się ich kariera gdyby nie nazwiska w rodzaju: Steven Spielberg, Peter Gabriel, Nigel Kennedy, David Lynch...
+                </div>
+
+            </div><br>
+
+            <div class="event">
+                <div class="eventL" style="float: left;"> <img src="img/macy.jpg" height="140" /> </div>
+                <div class="tekstR"> <br>
+                    <strong> MACY GRAY - </strong> Jedna z największych gwiazd neo soul, wokalistka o bardzo charakterystycznym, chrypliwym, i ciepłym głosie. Zadebiutowała w 1999 roku albumem „On How Life Is”, z którego pochodzi m.in. przebojowy utwór „I Try”.
+                </div>
+
+            </div><br>
+
+            <div class="event">
+                <div class="eventL" style="float: left;"> <img src="img/pat.jpg" height="140" padding /> </div>
+                <div class="tekstR"> <br>
+                    <strong>Pat Metheny - </strong> Jeden z najbardziej rozpoznawalnych muzyków jazzowych świata. Nieprzerwanie od 1976 roku wydaje kolejne albumy, solowe, w projekcie Pat Metheny Group oraz w towarzystwie innych artystów (m.in. Brad Mehldau, Herbie Hancock, David Bowie, Steve Reich, Ornette Coleman, Jim Hall, Milton Nascimento).
+                </div>
+
+            </div><br>
+
+
+
+
+
+
+        </div>
+
+        <div id="navYT"> <!-- możesz potem zmienić nazwę -->
+
+            <p><strong>TOP artyści &#127925 </strong></p>
+            <br>
+
+            <!-- 
+
+            <a href="https://youtu.be/9HUV5a7MgS4?si=5M87Ej4BrjcX_mSj" target="_blank"> 
+                Lianne la Havas </a>
+            <br /> <br /> 
+
+            -->
+
+            <div class="singer">
+                <a href="https://youtu.be/9HUV5a7MgS4?si=5M87Ej4BrjcX_mSj" target="_blank" title="Lianne la Havas">
+                    <img src="img/lianne.jpg" width="140" /> </a>
+            </div> <br>
+
+            <div class="singer">
+                <a href="https://youtu.be/ffzbacDuawI?si=SC7Ho84TXR3IkzX5" target="_blank" title="Tom Misch">
+                    <img src="img/tom misch.jpg" width="140" /> </a>
+            </div> <br>
+
+            <div class="singer">
+                <a href="https://youtu.be/v6xUgawDQB0?si=bJEUXa97L7OIiAMi" target="_blank" title="Justin Timberlake">
+                    <img src="img/justin.jpg" width="140" /> </a>
+            </div> <br>
+
+            <div class="singer">
+                <a href="https://youtu.be/GujTYAgG0Tc?si=IYw6Hf4bl5XsdPuB" target="_blank" title="Zak Abel">
+                    <img src="img/zak abel.jpg" width="140" /> </a>
+            </div> <br>
+
+            <div class="singer">
+                <a href="https://youtu.be/T6QKqFPRZSA?si=hNWBxugOhglX2Twd" target="_blank" title="Layryn Hill">
+                    <img src="img/lauryn.jpg" width="140" /> </a>
+            </div> <br>
+
+            <div class="singer">
+                <a href="https://youtu.be/qvEbZKiwYgo?si=mBtJNznc40Pgtcol" target="_blank" title="Honne">
+                    <img src="img/honne.jpg" width="140" /> </a>
+            </div> <br>
+
+
+
+
+        </div>
+
+
+
+        <div id="footer">
+            Najlepsza wyszukiwarka dla Ciebie! &copy; Wszelkie prawa zastrzeżone
+
+
+        </div>
+
+
+
+    </div>
+
+
+</body>
+
+</html>
